9dba8d8bb1fc786e1f278951eb4ed65de7302edf
Miguel Carvajal <miguel.carvajal@armadilloamarillo.com>
2019-03-29 11:38:45 +0100

    added motos.net

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let async = require('async');
 let segundamano = require('./segundamano');
 let motosnet = require('./motosnet');
 async.series([
-    // segundamano.retrieveAds.bind(null, 'cbr', 'madrid', 'honda')
+    segundamano.retrieveAds.bind(null, 'cbr', 'madrid', 'honda'),
     motosnet.retrieveAds.bind(null, 'cbr', 'madrid', 'honda')
 ], (err, results) => {
     if (err) return console.log(err);
